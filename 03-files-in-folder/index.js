4f01c816bfc80c548e233bf57b3684cc340f43b1
Alexander Bychyk <sanya55bychyk@gmail.com>
2022-05-21 15:16:27 +0300

    feat: finish 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+const dir = path.join(__dirname, 'secret-folder');
+showDir(dir);
+
+function showDir(dir) {
+  fs.readdir(dir, (err, files) => {
+    files.forEach(file => {
+      let newDir = path.join(dir, file);
+      fs.stat(newDir, (err, stats) => {
+        if (stats.isFile()) {
+          console.log([
+            file.slice(0, file.lastIndexOf('.')),
+            file.slice(file.lastIndexOf('.')),
+            `${stats.size * 0.001}kb`
+          ].join(' - '));
+        } else {
+          showDir(newDir);
+        }
+      });
+    });
+  });
+}
