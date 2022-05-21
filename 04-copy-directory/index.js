b1726c14fa4e58e95f90dea41894390658997365
Alexander Bychyk <sanya55bychyk@gmail.com>
2022-05-21 17:17:24 +0300

    feat: finish 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,15 @@
+const fs = require('fs');
+const path = require('path');
 
+const dir = path.join(__dirname, 'files');
+const dirCopy = path.join(__dirname, 'copyDir/');
+
+fs.rmdir(dirCopy, () => {});
+fs.mkdir(dirCopy, () => {});
+fs.readdir(dir, (err, data) => {
+  data.forEach(v => {
+    let fileDir = path.join(dir, v);
+    let fileCopyDir = path.join(dirCopy, v);
+    fs.copyFile(fileDir, fileCopyDir, () => {});
+  });
+});
